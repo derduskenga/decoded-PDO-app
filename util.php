ae8f0f7b62addcaac4dc11458a10baa696a6f701
derduskenga <derduskenga@gmail.com>
2020-11-02 12:32:26 +0300

    Update util.php

--- util.php
+++ util.php
@@ -2,10 +2,10 @@
 	class Util{
 		
 		//About DB
-		static $DB_NAME = "decode";
-		static $DB_USER = "postgres";
+		static $DB_NAME = "";
+		static $DB_USER = "";
 		//Use password you set to postgres user
-		static $DB_USER_PASS = "slimkk33..";
-		static $SERVER_NAME = "localhost";
+		static $DB_USER_PASS = "";
+		static $SERVER_NAME = "";
 	}
 ?>
